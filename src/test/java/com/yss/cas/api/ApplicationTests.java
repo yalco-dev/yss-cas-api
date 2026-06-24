3efce9ebe253427820993814a270d57c877d1ddc
Yalchan Niyaziev <yalchan.niyaziev@sirma.bg>
2026-06-24 17:54:44 +0300

    Initial project

---
+++ src/test/java/com/yss/cas/api/ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yss.cas.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
